c404e87f01475c27b04c18c4894245510121aeb6
Roberto Gomes <robertohgm@hotmail.com>
2019-08-06 22:22:34 -0300

    Formatação de gridlines

---
+++ src/SalesOrderWpf.UI/Helpers/FormHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SalesOrderWpf.UI.Helpers
+{
+    public static class FormHelper
+    {
+        public static void MessageError(Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Mensagem do Sistema", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public static void MessageSuccess(string message = "Operação realizada com sucesso")
+        {
+            MessageBox.Show(message, "Mensagem do Sistema", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static void SetHeader(this DataGrid dataGrid, string columnName, string columnDescription)
+        {
+            var column = dataGrid.Columns.FirstOrDefault(c => columnName.Equals(c.Header.ToString()));
+
+            if (column is null) return;
+
+            column.Header = columnDescription;
+        }
+    }
+}
--- src/SalesOrderWpf.UI/OrdersForm.xaml.cs
+++ src/SalesOrderWpf.UI/OrdersForm.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SalesOrderWpf.Domain.ViewModels;
+using SalesOrderWpf.UI.Helpers;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +22,56 @@ namespace SalesOrderWpf.UI
     /// </summary>
     public partial class OrdersForm : Window
     {
+        public ObservableCollection<LineInput> Lines { get; set; }
+
         public OrdersForm()
         {
             InitializeComponent();
+            Lines = new ObservableCollection<LineInput>();
+            gridLines.ItemsSource = Lines;
+            SignEvents();
+        }
+
+        private void SignEvents()
+        {
+            this.Loaded += Window_Loaded;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FormatGrid();
+            }
+            catch (Exception ex)
+            {
+                FormHelper.MessageError(ex);
+            }
+        }
+
+        private void FormatGrid()
+        {
+            gridLines.SetHeader("Id", "Código");
+            gridLines.SetHeader("OrderId", "Pedido");
+            gridLines.SetHeader("ItemCode", "Código Item");
+            gridLines.SetHeader("ItemName", "Descrição");
+            gridLines.SetHeader("Price", "Preço");
+            gridLines.SetHeader("Quantity", "Quantidade");
+            gridLines.SetHeader("Total", "Total");
+            gridLines.IsReadOnly = true;
+        }
+
+        private void CmdOk_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var est = Lines;
+                Lines.Add(new LineInput { ItemCode = "I0002", ItemName = "Clip", Price = 13.22m, Quantity = 5, Total = 57.86m });
+            }
+            catch (Exception ex)
+            {
+                FormHelper.MessageError(ex);
+            }
         }
     }
 }
